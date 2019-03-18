cd12794352ea3d128883decf92dd37b1dcebc358
3Permata <tigapermatasystem.3@gmail.com>
2019-03-18 13:25:38 +0700

    coba

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     $host = "subsatuserver.database.windows.net";
     $user = "submission";
     $pass = "Anggie123";
-    $db = "dbo.Registration";
+    $db = "Registration";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
